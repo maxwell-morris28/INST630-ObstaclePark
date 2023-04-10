adac9b0e227e50656574df1f57b4532bdd665145
mmaxgamma <89859919+maxwell-morris28@users.noreply.github.com>
2023-04-10 16:04:35 -0400

    added character forward movement and camera offset

--- ObstaclePark/Scripts/levelOne.js
+++ ObstaclePark/Scripts/levelOne.js
@@ -1,4 +1,7 @@
 import * as THREE from "three";
+//import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+const cameraOffset = new THREE.Vector3(0.0, 5.0, 20.0); // NOTE Constant offset between the camera and the target
 
 //Creating the scene that holds all 
 const scene = new THREE.Scene();
@@ -7,6 +10,8 @@ scene.background = new THREE.Color(0x24252a);
 
 //Perspective Camera
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight , 0.1, 1000);
+
+
 
 //instantiate renderer and map it to existing canvas
 const renderer = new THREE.WebGLRenderer({
@@ -17,37 +22,57 @@ const canvas = document.querySelector('#field');
 
 const geometry = new THREE.BoxGeometry(3, 3, 3);
 const material = new THREE.MeshBasicMaterial( {color: 0xFF6347});
-//const torus = new THREE.Mesh(geometry, material);
 const player = new THREE.Mesh(geometry, material);
 
-//scene.add(torus);
 scene.add(player);
+
+player.translateY(2);
+
+
+const plane = new THREE.PlaneGeometry(100, 1000);
+const ground_color = new THREE.MeshBasicMaterial( {color: 0xD3D3D3});
+const ground = new THREE.Mesh(plane, ground_color);
+
+scene.add(ground);
 
 
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
-//document.body.appendChild( renderer.domElement );
 
-camera.position.setZ(30);
+ground.rotateX(- Math.PI / 2);
 
 renderer.render(scene, camera);
 
 canvas.height=window.innerHeight;
 canvas.width=window.innerWidth;
 
+//const controls = new OrbitControls(camera, renderer.domElement);
+
+camera.position.set(0,7,20); // Set position like this
+//controls.update();
+
+const objectPosition = player.position;
 
 function animate() {
     requestAnimationFrame(animate);
 
-    player.rotation.y += .01;
+    player.getWorldPosition(objectPosition);
+    //console.log(player.getWorldPosition(objectPosition));
+  
+    camera.position.copy(objectPosition).add(cameraOffset);
+    console.log(camera.position);
 
+    player.position.z -= 1;
+    
     canvas.height=window.innerHeight;
     canvas.width=window.innerWidth;
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(window.innerWidth, window.innerHeight);
-    //camera.position.setZ(30);
+
+    //controls.update();
 
     renderer.render(scene, camera);
 }
 
 animate();
+
