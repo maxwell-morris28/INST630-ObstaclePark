7b9feec98891798194183e995248b71f4174812d
mmaxgamma <89859919+maxwell-morris28@users.noreply.github.com>
2023-04-14 21:45:57 -0400

    added character movement, textures and left/right boundaries

--- ObstaclePark/Scripts/levelOne.js
+++ ObstaclePark/Scripts/levelOne.js
@@ -7,6 +7,11 @@ const cameraOffset = new THREE.Vector3(0.0, 5.0, 20.0); // NOTE Constant offset 
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0x24252a);
 
+const loader = new THREE.TextureLoader();
+loader.load('pexels-hristo-fidanov-1252890.jpg' , function(texture)
+            {
+             scene.background = texture;  
+            });
 
 //Perspective Camera
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight , 0.1, 1000);
@@ -28,9 +33,9 @@ scene.add(player);
 
 player.translateY(2);
 
-
+const groundTexture = new THREE.TextureLoader().load('pexels-pixabay-69927.jpg');
 const plane = new THREE.PlaneGeometry(100, 1000);
-const ground_color = new THREE.MeshBasicMaterial( {color: 0xD3D3D3});
+const ground_color = new THREE.MeshBasicMaterial( {map: groundTexture});
 const ground = new THREE.Mesh(plane, ground_color);
 
 scene.add(ground);
@@ -60,7 +65,7 @@ function animate() {
     //console.log(player.getWorldPosition(objectPosition));
   
     camera.position.copy(objectPosition).add(cameraOffset);
-    console.log(camera.position);
+    //console.log(camera.position);
 
     player.position.z -= 1;
     
@@ -70,8 +75,42 @@ function animate() {
     renderer.setSize(window.innerWidth, window.innerHeight);
 
     //controls.update();
-
+    keyPressed();
+    if(leftDown && player.position.x > -48) {
+        console.log(player.position.x);
+        player.position.x -=1.5;
+    }
+    else if(rightDown && player.position.x < 48) {
+        console.log(player.position.x);
+        player.position.x +=1.5;
+    }
     renderer.render(scene, camera);
+}
+
+let leftDown = false;
+let rightDown = false;
+
+function keyPressed() {
+    document.addEventListener("keydown", function(event) {
+        //console.log(event.code);
+        if(event.code === "KeyA") {
+            leftDown = true;
+        }
+        else if(event.code === "KeyD") {
+            rightDown = true;
+        }
+    }) 
+
+    document.addEventListener("keyup", function(event) {
+        console.log(event.code);
+        if(event.code === "KeyA") {
+            leftDown = false;
+        }
+        else if(event.code === "KeyD") {
+            rightDown = false;
+        }
+    }) 
+
 }
 
 animate();
