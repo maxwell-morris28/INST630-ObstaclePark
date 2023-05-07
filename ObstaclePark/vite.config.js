aba781446c2bfa53ee08ccbec0eb46c9d2e03f52
mmaxgamma <89859919+maxwell-morris28@users.noreply.github.com>
2023-05-07 12:28:47 -0400

    reverted paths

--- ObstaclePark/vite.config.js
+++ ObstaclePark/vite.config.js
@@ -4,6 +4,4 @@ export default defineConfig({
   // config options
   build: {
     target: 'esnext' //browsers can handle the latest ES features
-  },
-  base: '/https://maxwell-morris28.github.io/INST630-ObstaclePark/ObstaclePark/'
-})
+  }})
